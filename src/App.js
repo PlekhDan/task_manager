6dee3f8d28773a0fab4de447752166c048fd1483
Ilgam911 <ilgamgilaziev@gmail.com>
2021-04-08 15:35:55 +0300

    регистрация+маршрут в app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Route} from "react-router-dom";
 import {Menu} from "./components/Menu";
 import {Welcome} from "./components/Welcome";
 import {Dashboard} from "./components/Dashboard";
+import {Reg} from "./components/Reg";
 
 function App() {
     return (
             <BrowserRouter>
                 <Route exact path="/" render={() => <Menu/>}/>
                 {/*<Route path="/create" render={() =>  }/>*/}
+                <Route path="/reg" render={()=><Reg/>}/>
                 <Welcome/>
                 <Dashboard/>
             </BrowserRouter>
