d2dd8ad3f05fb6f4f9f3d80b8aadbd4eef60b7c2
Ilgam911 <ilgamgilaziev@gmail.com>
2021-04-12 22:00:13 +0300

    возвращает на главную страницу

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Redirect} from "react-router-dom";
 
 export class Reg extends React.Component{
     constructor(props) {
@@ -11,7 +11,9 @@ export class Reg extends React.Component{
             lastname:"",
             email:"",
             pass:"",
-            info:""
+            info:"",
+            redirect: false,
+            submitBtn: "disabled"
         }
     }
 
@@ -26,14 +28,23 @@ export class Reg extends React.Component{
             method: "POST",
             body: formData
         }).then(response=>response.json())
-            .then(result=>{
-                console.log(result)
+            .then(result=> {
+                this.setState({
+                    redirect: true
+                })
             })
     }
     handleInputChange(event){
         const value = event.target.value;
         const name = event.target.name;
+        this.setState({
+            [name]: value
+        })
         if (name === "email"){
+            if (value === "") {
+                this.setState({submitBtn: "disabled"});
+                return;
+            }
             const formData = new FormData();
             formData.append("email",value);
             fetch("http://o90576od.beget.tech/checkReg",{
@@ -43,20 +54,22 @@ export class Reg extends React.Component{
                 .then(result=>{
                     if(result.result === "exist"){
                         this.setState({
-                            info: "Такая почта существует!"
+                            info: "Такая почта существует!",
+                            submitBtn: "disabled"
                         })
                     }else{
                         this.setState({
-                            info: ""
+                            info: "",
+                            submitBtn: ""
                         })
                     }
                 });
         }
-        this.setState({
-            [name]:value
-        })
     }
     render() {
+        if(this.state.redirect)
+            return <Redirect to="/" />
+        else
         return <div className="col-sm-5 my-3 mx-auto">
             <form onSubmit={this.sendForm}>
                 <div className="mb-3">
@@ -73,7 +86,7 @@ export class Reg extends React.Component{
                     <input value={this.state.pass} onChange={this.handleInputChange} name="pass" type="password" className="form-control" placeholder="Пароль"/>
                 </div>
                 <div className="mb-3">
-                    <input type="submit" className="form-control btn btn-primary" value="Регистрация"/>
+                    <input type="submit" disabled={this.state.submitBtn} className="form-control btn btn-primary" value="Регистрация"/>
                 </div>
             </form>
         </div>
