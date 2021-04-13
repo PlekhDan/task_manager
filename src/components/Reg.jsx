ff4d92bc0cdd805bf255e16ac4ae44143ea412ee
Ilgam911 <ilgamgilaziev@gmail.com>
2021-04-13 15:05:52 +0300

    возвращает на главную страницу

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -4,8 +4,8 @@ import {Redirect} from "react-router-dom";
 export class Reg extends React.Component{
     constructor(props) {
         super(props);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.sendForm = this.sendForm.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
         this.state = {
             name:"",
             lastname:"",
@@ -15,9 +15,16 @@ export class Reg extends React.Component{
             redirect: false
         }
     }
+    handleInput(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]:value
+        })
+    }
 
-    sendForm(event){
-        event.preventDefault();
+    handleSubmit(e){
+        e.preventDefault();
         const formData = new FormData();
         formData.append("name",this.state.name);
         formData.append("lastname",this.state.lastname);
@@ -26,57 +33,39 @@ export class Reg extends React.Component{
         fetch("http://o90576od.beget.tech/handlerReg",{
             method: "POST",
             body: formData
-        }).then(response=>response.json())
-            .then(result=> {
-                this.setState({
-                    redirect: true
-                })
-            })
-    }
-    handleInputChange(event){
-        const value = event.target.value;
-        const name = event.target.name;
-        if (name === "email"){
-            const formData = new FormData();
-            formData.append("email",value);
-            fetch("http://o90576od.beget.tech/checkReg",{
-                method: "POST",
-                body: formData
-            }).then(response=>response.json())
-                .then(result=>{
-                    if(result.result === "exist"){
-                        this.setState({
-                            info: "Такая почта существует!"
-                        })
-                    }else{
-                        this.setState({
-                            info: ""
-                        })
-                    }
-                });
-        }
-        this.setState({
-            [name]: value
         })
+            .then(response=>response.json())
+            .then(result=>{
+                if(result.result === "exist"){
+                    this.setState({
+                        info: "Такая почта существует!"
+                    })
+                }else{
+                    this.setState({
+                        redirect: true
+                    })
+                }
+            });
     }
+
     render() {
         if(this.state.redirect)
             return <Redirect to="/" />
         else
         return <div className="col-sm-5 my-3 mx-auto">
-            <form onSubmit={this.sendForm}>
+            <form onSubmit={this.handleSubmit}>
                 <div className="mb-3">
-                    <input value={this.state.name} onChange={this.handleInputChange} name="name" type="text" className="form-control" placeholder="Имя"/>
+                    <input value={this.state.name} onChange={this.handleInput} name="name" type="text" className="form-control" placeholder="Имя"/>
                 </div>
                 <div className="mb-3">
-                    <input value={this.state.lastname} onChange={this.handleInputChange} name="lastname" type="text" className="form-control" placeholder="Фамилия" />
+                    <input value={this.state.lastname} onChange={this.handleInput} name="lastname" type="text" className="form-control" placeholder="Фамилия" />
                 </div>
                 <div className="mb-3">
-                    <input value={this.state.email} onChange={this.handleInputChange} name="email" type="email" className="form-control" placeholder="Email"/>
+                    <input value={this.state.email} onChange={this.handleInput} name="email" type="email" className="form-control" placeholder="Email"/>
                     <p style={{color:"red"}}>{this.state.info}</p>
                 </div>
                 <div className="mb-3">
-                    <input value={this.state.pass} onChange={this.handleInputChange} name="pass" type="password" className="form-control" placeholder="Пароль"/>
+                    <input value={this.state.pass} onChange={this.handleInput} name="pass" type="password" className="form-control" placeholder="Пароль"/>
                 </div>
                 <div className="mb-3">
                     <input type="submit" className="form-control btn btn-primary" value="Регистрация"/>
