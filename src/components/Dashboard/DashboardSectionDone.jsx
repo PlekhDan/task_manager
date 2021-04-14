ebeaddb1533291ad98fbb169fce5ba39059c9c01
Daria <Plekhanova.danya@gmail.com>
2021-04-14 14:14:52 +0300

    Menu

--- src/components/Dashboard/DashboardSectionDone.jsx
+++ src/components/Dashboard/DashboardSectionDone.jsx
@@ -29,7 +29,8 @@ export class DashboardSectionDone extends React.Component {
 
     render() {
         return (
-            <div className={s.block}>
+            // <div className={s.block}>
+            <div className="border border-primary rounded px-4 text-center">
                 <h4 className="py-4">Done</h4>
                 {this.state.tasks}
             </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -18,31 +18,24 @@ export class Menu extends React.Component {
     }
 
     componentDidMount() {
-        if (false) { // если путь === "/dashboard"
+        if (true) { // если путь === "/dashboard"
             this.setState({
-                create: <NavLink className="btn btn-outline-success me-3 mb-2" to="/create" type="submit">Создать
-                    задачу</NavLink>,
+                create: <NavLink className="btn btn-outline-success me-3 my-1 text-nowrap" to="/create" type="submit">Создать задачу</NavLink>,
             })
         }
-        if (false) { // если путь === "/dashboard"
+        if (true) { // если путь === "/dashboard"
             this.setState({
-                cabinet: <NavLink className="btn btn-outline-success me-3 mb-2" to="/cabinet" type="submit">Личный
-                    кабинет</NavLink>,
+                cabinet: <NavLink className="btn btn-outline-success me-3 my-1 text-nowrap" to="/cabinet" type="submit">Личный кабинет</NavLink>,
             })
         }
         if (true) { // если путь === "/"  || путь === "/auth"
             this.setState({
-                reg: <NavLink className="btn btn-outline-primary me-3 my-2" to="/reg" type="submit">Регистрация</NavLink>,
+                reg: <NavLink className="btn btn-outline-primary me-3 my-1" to="/reg" type="submit">Регистрация</NavLink>,
             })
         }
         if (true) { // если путь === "/"  || путь === "/reg"
             this.setState({
-                auth: <NavLink className="btn btn-outline-primary me-3 my-2" to="auth" type="submit">Войти</NavLink>,
-            })
-        }
-        if (true) { // если путь === "/"  || путь === "/create"
-            this.setState({
-                create: <NavLink className="btn btn-outline-success me-3 my-2" to="/create" type="submit">Создать задачу</NavLink>,
+                auth: <NavLink className="btn btn-outline-primary me-3 my-1" to="/auth" type="submit">Войти</NavLink>,
             })
         }
     }
@@ -51,46 +44,28 @@ export class Menu extends React.Component {
         return (
             <div className="mb-5">
 
-                {/*<nav className="navbar navbar-expand-md navbar-light bg-light rounded-bottom shadow-lg p-3 mb-5 bg-white rounded">*/}
-                {/*    <div className="container">*/}
-                {/*        <a className="navbar-brand me-3 mb-2" href="/"><h1>tsk_mngr</h1></a>*/}
-                {/*        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"*/}
-                {/*                data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"*/}
-                {/*                aria-label="Toggle navigation">*/}
-                {/*            <span className="navbar-toggler-icon"></span>*/}
-                {/*        </button>*/}
-                {/*        <div className="collapse navbar-collapse" id="navbarText">*/}
-                {/*            {this.state.create}*/}
-                {/*            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>*/}
-                {/*            {this.state.reg}*/}
-                {/*            {this.state.auth}*/}
-                {/*            {this.state.cabinet}*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</nav>*/}
-
-                <nav className="navbar navbar-expand-lg navbar-light shadow-sm">
+                <nav className="navbar navbar-expand-md navbar-light bg-light rounded-bottom shadow p-3 mb-5 bg-white rounded">
                     <div className="container">
-                        <NavLink className="navbar-brand" to="/">
-                            <h1>tsk_mngr</h1>
-                        </NavLink>
+                        <a className="navbar-brand me-3 mb-2" href="/"><h1>tsk_mngr</h1></a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
+                                data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
+                                aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"/>
                         </button>
-                        <div className=" collapse navbar-collapse py-3" id="navbarSupportedContent">
-                            <div className="me-auto"/>
-                                <div className="d-grid gap-2 d-md-flex">
-                                    {this.state.reg}
-                                    {this.state.auth}
-                                    {/*{this.state.cabinet}*/}
-                                    {/*{this.state.create}*/}
-                                </div>
+                        <div className="collapse navbar-collapse" id="navbarText">
+                            <div className="d-grid gap-2 d-md-flex">
+                                {this.state.create}
+                            </div>
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0"/>
+                            <div className="d-grid gap-2 d-md-flex">
+                                {this.state.reg}
+                                {this.state.auth}
+                                {this.state.cabinet}
+                            </div>
+
                         </div>
                     </div>
                 </nav>
-
             </div>
         );
     }
