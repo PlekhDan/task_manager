4a0f3170e72c35a8f54e7bf29a58b76c115dd840
Daria <Plekhanova.danya@gmail.com>
2021-04-21 10:46:47 +0300

    Tasks.php and router.php

--- public/task_php/classes/Tasks.php
+++ public/task_php/classes/Tasks.php
@@ -1,46 +1,43 @@
 <?
-class Task{
-    
-  static function getTasks(){
-      global $mysqli;
-      $result = $mysqli->query("SELECT tasks.id, tasks.title, tasks.text, tasks.date_added FROM `tasks`,`clients` WHERE clients.id = tasks.userid"); // Запрашиваем все посты из БД
-      $tasks = [];
+class Tasks{
+
+  static function getStatus(){
+    global $mysqli;
+	$result = $mysqli->query("SELECT * FROM `status`");
+	$status = [];
+	  while($row = $result->fetch_assoc()){
+	    $status[] = $row;
+	  }
+	  echo json_encode($status);
+  }
+
+  static function getOneStatus($id){
+    global $mysqli;
+	$result = $mysqli->query("SELECT tasks.id, tasks.title, tasks.text, tasks.date_added, status.value AS statusid FROM `tasks`,`clients`,`status` WHERE clients.id = tasks.userid AND status.id=tasks.statusid AND status.id=$id");
+    $status = [];
       while($row = $result->fetch_assoc()){
-        $tasks[] = $row;
+        $status[] = $row;
       }
-      echo json_encode($tasks);
+      echo json_encode($status);
   }
-  
-  static function getTask($id){
+
+  static function changeStatus($statusId, $id){
     global $mysqli;
-    $result = $mysqli->query("SELECT * FROM tasks WHERE id=$id"); 
-    echo json_encode($result->fetch_assoc());
-  }
-  
-  static function addTask($title,$text){
-    global $mysqli;
-    $title = $_POST['title'];
-    $text = $_POST['text'];
-    $mysqli->query("INSERT INTO `tasks`(`title`, `text`) VALUES ('$title','$text')");
+    $mysqli->query("UPDATE `tasks` SET `statusid`=$statusId WHERE id=$id");
     echo json_encode(['result'=>'success']);
   }
-  
+
+  static function getIdTask($id){
+    global $mysqli;
+    $result = $mysqli->query("SELECT tasks.id, tasks.title, tasks.text, tasks.date_added, status.value AS statusid, clients.email AS userid FROM `tasks`,`clients`,`status` WHERE clients.id = tasks.userid AND status.id = tasks.statusid AND tasks.id=$id");
+	echo json_encode($result->fetch_assoc());
+  }
+
   static function removeTask($id){
     global $mysqli;
-    $id = $_POST['id'];
-    $mysqli->query("DELETE FROM `tasks` WHERE id=$id;");
+    $mysqli->query("DELETE FROM `tasks` WHERE id=$id");
     echo json_encode(['result'=>'success']);
   }
-  
-  static function changeTask(){
-    global $mysqli;
-    session_start();
-    $value = $_POST['value'];
-    $item = $_POST['item']; 
-    $id = $_SESSION['id'];
-    $mysqli->query("UPDATE `tasks` SET `$item`='$value' WHERE `id`=$id");
-    $_SESSION[$item] = $value;
-    echo json_encode(['result'=>'success']);
-  }    
+
 }
 ?>
