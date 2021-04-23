09684ed5cf9d7bb7ebf8ec0177aa44539afaab1b
= <=>
2021-04-23 23:32:22 +0300

    Показ кнопок в Меню в зависимости от сессии (третья итерация)

--- src/components/Exit.jsx
+++ src/components/Exit.jsx
@@ -8,7 +8,8 @@ export class Exit extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            menu: ""
+            menu: "",
+            body: ""
         }
     }
     componentDidMount() {
@@ -20,7 +21,20 @@ export class Exit extends React.Component {
             .then(result=>{
                 if(result.result == "success"){
                     this.setState({
-                        menu: <Menu/>
+                        menu: <Menu/>,
+                        body: <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <div className={s.blockModule}>
+                                        <div className={s.bord}>
+                                            <h1 className={s.text}>Возвращайтесь!</h1>
+                                        </div>
+                                        <div className={s.img}/>
+                                        {/*<img src="https://www.mota.ru/upload/resize/2560/1600/upload/wallpapers/source/2014/04/16/20/01/39936/hytT7JJUHd-ca5.jpg" className="d-block w-100" alt="..."/>*/}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     })
                 }
 
@@ -29,19 +43,7 @@ export class Exit extends React.Component {
     render() {
         return ( <div>
                 {this.state.menu}
-                    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <div className={s.blockModule}>
-                                    <div className={s.bord}>
-                                        <h1 className={s.text}>Возвращайтесь!</h1>
-                                    </div>
-                                    <div className={s.img}/>
-                                    {/*<img src="https://www.mota.ru/upload/resize/2560/1600/upload/wallpapers/source/2014/04/16/20/01/39936/hytT7JJUHd-ca5.jpg" className="d-block w-100" alt="..."/>*/}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                {this.state.body}
                 </div>
 
 
