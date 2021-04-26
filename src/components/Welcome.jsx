cbd7c8706e5b1fad0b670ce38fdfb85b97a61b10
Daria <Plekhanova.danya@gmail.com>
2021-04-26 17:21:07 +0300

    Обновленные welcome и exit.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,19 +5,30 @@ import sample from "./1.mp4"
 
 export class Welcome extends React.Component {
     render() {
-        return(
+        return (
+            // <div>
+            //     <Menu />
+            //         <div>
+            //             <div className={s.fullscreen}>
+            //                 <div className={s.body}>
+            //                     <div className={s.title}>Добро пожаловать</div>
+            //                 </div>
+            //                 <video autoPlay muted loop className={s.video}>
+            //                     <source type="video/mp4" src={sample}/>
+            //                 </video>
+            //             </div>
+            //         </div>
+            // </div>
+
             <div>
                 <Menu />
-                    <div>
-                        <div className={s.fullscreen}>
-                            <div className={s.body}>
-                                <div className={s.title}>Добро пожаловать</div>
-                            </div>
-                            <video autoPlay muted loop className={s.video}>
-                                <source type="video/mp4" src={sample}/>
-                            </video>
-                        </div>
-                    </div>
+                <div className={s.outer}>
+                    <div className={s.title}>Добро пожаловать</div>
+                    <video autoPlay muted loop className={s.video}>
+                        <source type="video/mp4" src={sample}/>
+                    </video>
+                </div>
+
             </div>
         );
     }
