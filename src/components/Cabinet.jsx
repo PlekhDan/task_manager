dab63dbebe4c6911a06bc83114ab9aa41579f51a
Daria <Plekhanova.danya@gmail.com>
2021-04-26 20:30:23 +0300

    cabinet

--- src/components/Cabinet.jsx
+++ src/components/Cabinet.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import {host} from "../config";
 import {CabinetTask} from "./CabinetTask";
 import {Menu} from "./Menu";
+import {Task} from "./Dashboard/Task";
 
 
-
-
-export class Cabinet extends React.Component{
+export class Cabinet extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -18,8 +17,9 @@ export class Cabinet extends React.Component{
             redirect: false
         }
         this.handleInput = this.handleInput.bind(this);
-        this.handleSubmit= this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleInput(e) {
         const name = e.target.name;
         const value = e.target.value;
@@ -28,19 +28,19 @@ export class Cabinet extends React.Component{
         })
     }
 
-    handleSubmit(e){
+    handleSubmit(e) {
         e.preventDefault();
         const formData = new FormData();
-        formData.append("name",this.state.name);
-        formData.append("lastname",this.state.lastname);
-        fetch(host + "/handlerChangeUserData",{
+        formData.append("name", this.state.name);
+        formData.append("lastname", this.state.lastname);
+        fetch(host + "/handlerChangeUserData", {
             method: "POST",
             body: formData,
             credentials: 'include'
         })
-            .then(response=>response.json())
-            .then(result=>{
-                if(result.result === "success"){
+            .then(response => response.json())
+            .then(result => {
+                if (result.result === "success") {
                     this.setState({
                         redirect: true
                     })
@@ -51,12 +51,12 @@ export class Cabinet extends React.Component{
     }
 
     componentDidMount() {
-        fetch(host + "/getUser",{/* тут для получения данных о сессии в БД*/
+        fetch(host + "/getUser", {/* тут для получения данных о сессии в БД*/
             credentials: 'include'
         })
-            .then(response=>response.json())
-            .then(result=>{
-                if(result.result !== "error"){
+            .then(response => response.json())
+            .then(result => {
+                if (result.result !== "error") {
                     this.setState({
                         fname: result.name,
                         flastname: result.lastname
@@ -70,29 +70,40 @@ export class Cabinet extends React.Component{
         if (this.state.redirect === true)
             return <Cabinet/>;
         else
-            return(
+            return (
                 <div>
                     <Menu/>
                     <div className="container">
-                        <div className="row">
-                            <div className="col-sm-6 my-3 border rounded h-50 shadow-sm bg-white pb-3">
-                                <h4 className="mb-3 py-3 rounded shadow-sm text-center">ЛИЧНЫЕ ДАННЫЕ</h4>
-                                <div className="row">
-                                    <div className="col-sm">
-                                        <form onSubmit={this.handleSubmit}>
-                                            <div className="input-group mb-2 p-1">
-                                                <span  className="input-group-text" id="basic-addon1">{this.state.fname}</span>
-                                                <input value={this.state.name} onChange={this.handleInput} type="text" className="form-control" name="name" placeholder="Имя"  aria-label="Username" aria-describedby="basic-addon1"/>
-                                            </div>
-                                            <div className="input-group mb-2 p-1">
-                                                <span className="input-group-text" id="basic-addon1">{this.state.flastname}</span>
-                                                <input value={this.state.lastname} onChange={this.handleInput} type="text" className="form-control" name="lastname" placeholder="Фамилия" aria-label="Username" aria-describedby="basic-addon1"/>
-                                            </div>
-                                            <div className="p-1">
-                                                <input disabled={!this.state.lastname || !this.state.name} type="submit" className="btn btn-dark bg-gradient" value="Сохранить изменения"/>
-                                            </div>
-
-                                        </form>
+                        <div className="d-flex bg-info">
+                            <div className="col-sm-6">
+                                <div className="p-3 h-100">
+                                    <div className="w-100 h-50 border rounded shadow-sm pb-3 mb-3 bg-white rounded">
+                                        <h4 className="mb-3 py-3 rounded shadow-sm text-center">ЛИЧНЫЕ ДАННЫЕ</h4>
+                                        <div className="mx-3">
+                                            <form onSubmit={this.handleSubmit}>
+                                                <div className="input-group mb-3">
+                                                    <span className="input-group-text"
+                                                          id="basic-addon1">{this.state.fname}</span>
+                                                    <input value={this.state.name} onChange={this.handleInput}
+                                                           type="text" className="form-control" name="name"
+                                                           placeholder="Имя" aria-label="Username"
+                                                           aria-describedby="basic-addon1"/>
+                                                </div>
+                                                <div className="input-group mb-5">
+                                                    <span className="input-group-text"
+                                                          id="basic-addon1">{this.state.flastname}</span>
+                                                    <input value={this.state.lastname} onChange={this.handleInput}
+                                                           type="text" className="form-control" name="lastname"
+                                                           placeholder="Фамилия" aria-label="Username"
+                                                           aria-describedby="basic-addon1"/>
+                                                </div>
+                                                <div className="">
+                                                    <input disabled={!this.state.lastname || !this.state.name}
+                                                           type="submit" className="btn btn-dark bg-gradient"
+                                                           value="Сохранить изменения"/>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -100,6 +111,7 @@ export class Cabinet extends React.Component{
                                 <CabinetTask/>
                             </div>
                         </div>
+
                     </div>
                 </div>
 
